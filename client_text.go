fc7e63d98a22195a5c4e2dde1b9dd245260e19ad
connerdouglass <conner.douglass@gmail.com>
2023-03-06 09:58:44 -0500

    Added support for grammar correction APIs

--- client_text.go
+++ client_text.go
@@ -9,6 +9,16 @@ type TextClient interface {
 
 	// Summarize generates an AI summary of a given text input.
 	Summarize(ctx context.Context, req *SummarizeRequest) (*SummarizeResponse, error)
+
+	// Sentiment runs sentiment analysis on a given text input and rates the sentiment on a scale from "very_negative" to
+	// "very_positive".
+	Sentiment(ctx context.Context, req *SentimentRequest) (*SentimentResponse, error)
+
+	// ContentMatching determines how closely a given text input is relates to a list of content categories.
+	ContentMatching(ctx context.Context, req *ContentMatchingRequest) (*ContentMatchingResponse, error)
+
+	// Grammar corrects grammar in a given text input, and optionally explains the grammatical errors.
+	Grammar(ctx context.Context, req *GrammarRequest) (*GrammarResponse, error)
 }
 
 type MeasureRequest struct {
@@ -28,10 +38,49 @@ type SummarizeResponse struct {
 	Summary string `json:"summary"`
 }
 
+type SentimentRequest struct {
+	Text string `json:"text"`
+}
+
+type SentimentResponse struct {
+	Sentiment string `json:"sentiment"`
+}
+
+type ContentMatchingRequest struct {
+	Text       string   `json:"text"`
+	Categories []string `json:"categories"`
+}
+
+type ContentMatchingResponse struct {
+	Categories map[string]float32 `json:"categories"`
+}
+
+type GrammarRequest struct {
+	Text    string `json:"text"`
+	Explain *bool  `json:"explain"`
+}
+
+type GrammarResponse struct {
+	Text        string  `json:"text"`
+	Explanation *string `json:"explanation"`
+}
+
 func (c *client) Measure(ctx context.Context, req *MeasureRequest) (*MeasureResponse, error) {
 	return httpPost[MeasureResponse](ctx, c, "/solutions/text/v1/measure", req)
 }
 
 func (c *client) Summarize(ctx context.Context, req *SummarizeRequest) (*SummarizeResponse, error) {
 	return httpPost[SummarizeResponse](ctx, c, "/solutions/text/v1/summarize", req)
+}
+
+func (c *client) Sentiment(ctx context.Context, req *SentimentRequest) (*SentimentResponse, error) {
+	return httpPost[SentimentResponse](ctx, c, "/solutions/text/v1/sentiment", req)
+}
+
+func (c *client) ContentMatching(ctx context.Context, req *ContentMatchingRequest) (*ContentMatchingResponse, error) {
+	return httpPost[ContentMatchingResponse](ctx, c, "/solutions/text/v1/content-matching", req)
+}
+
+func (c *client) Grammar(ctx context.Context, req *GrammarRequest) (*GrammarResponse, error) {
+	return httpPost[GrammarResponse](ctx, c, "/solutions/text/v1/grammar", req)
 }
--- nulltypes.go
+++ nulltypes.go
@@ -1,5 +1,13 @@
 package apize
 
+func Ptr[T any](x T) *T {
+	return &x
+}
+
 func Int(x int) *int {
+	return &x
+}
+
+func Bool(x bool) *bool {
 	return &x
 }
